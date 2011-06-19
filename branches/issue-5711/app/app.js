a04f6f907c3dfe5ce0df10793d0855af4d6cd523
fam.lam@live.nl <fam.lam@live.nl@6bf221b2-0ac9-11df-a582-91aacf084772>
2011-06-19 19:48:22 +0000

    Update issue 5711 Status:ReadyForRelease Owner:fam.lam@live.nl Something like this should do the trick. However, you'll have to republish the app in order to have the convert-popup to work of course ;)
    After this is released, the only things that have to be done are
    1. delete /app/*
    2. delete the reference to app/app.js

---
+++ branches/issue-5711/app/app.js
@@ -0,0 +1,11 @@
+(function() {
+  if (SAFARI || /eefkobgceabjladaefihncnghgdiilge/.test(chrome.extension.getURL('')))
+    return;
+
+  chrome.browserAction.setIcon = function() {};
+  chrome.browserAction.setTitle = function() {};
+  chrome.browserAction.setBadgeText = function() {};
+  chrome.browserAction.setBadgeBackgroundColor = function() {};
+
+  chrome.windows.create({url:"app/app_EOL.html", type:"popup", width:400, height: 200});
+})();
