dfd944f3b203bb79dcf81326546dd7b2e8445de3
fam.lam@live.nl <fam.lam@live.nl@6bf221b2-0ac9-11df-a582-91aacf084772>
2012-01-06 00:02:35 +0000

    Update issue 6330 Status: NeedsReview Make url_parts (renamed to parseUri) "return" the javascript location object and parse the query, or return just a single property if specified.
    Advantages:
    - ports in URLs are parsed correctly
    - feed:https:// is parsed correctly (protocol = 'feed:')
    - many more options are available now, making it possible to use it in other parts of the code too (TODO)
    - The bug where it threw an error if url was undefined/"" is fixed
    - You can get the full location object or just a single property
    
    Disadvantages:
    - hard to read regex (however, it seemed to work totally fine on every absolute URL I checked, and if you temporary remove all 'do not capture' aka '?:', then it's better readable)
    
    Please review this first. After it has been approved and eventually merged into trunk, I'll start searching the code for other places where it can be used, where url_parts couldn't.

---
+++ branches/issue-6330/functions.js
@@ -0,0 +1,111 @@
+// Run a function on the background page.
+// Inputs (positional):
+//   first, a string - the name of the function to call
+//   then, any arguments to pass to the function (optional)
+//   then, a callback:function(return_value:any) (optional)
+BGcall = function() {
+  var args = [];
+  for (var i=0; i < arguments.length; i++)
+    args.push(arguments[i]);
+  var fn = args.shift();
+  var has_callback = (typeof args[args.length - 1] == "function");
+  var callback = (has_callback ? args.pop() : function() {});
+  chrome.extension.sendRequest({command: "call", fn:fn, args:args}, callback);
+}
+
+// These are replaced with console.log in adblock_start.js and background.html
+// if the user chooses.
+log = function() { };
+
+function translate(messageID, args) {
+  return chrome.i18n.getMessage(messageID, args);
+}
+
+function localizePage() {
+  //translate a page into the users language
+  $("[i18n]:not(.i18n-replaced)").each(function() {
+    $(this).html(translate($(this).attr("i18n")));
+  });
+  $("[i18n_value]:not(.i18n-replaced)").each(function() {
+    $(this).val(translate($(this).attr("i18n_value")));
+  });
+  $("[i18n_title]:not(.i18n-replaced)").each(function() {
+    $(this).attr("title", translate($(this).attr("i18n_title")));
+  });
+  $("[i18n_replacement_el]:not(.i18n-replaced)").each(function() {
+    // Replace a dummy <a/> inside of localized text with a real element.
+    // Give the real element the same text as the dummy link.
+    var dummy_link = $("a", this);
+    var text = dummy_link.text();
+    var real_el = $("#" + $(this).attr("i18n_replacement_el"));
+    real_el.text(text).val(text).replaceAll(dummy_link);
+    // If localizePage is run again, don't let the [i18n] code above
+    // clobber our work
+    $(this).addClass("i18n-replaced");
+  });
+}
+
+// Parse an URL. Based upon http://blog.stevenlevithan.com/archives/parseuri
+// parseUri 1.2.2, (c) Steven Levithan <stevenlevithan.com>, MIT License
+// Inputs: URL: the URL you want to parse
+//         part (optional): if you want a specific part of the URL, specify the
+//                          part name here. Must be one in the variable 'keys'
+// Outputs: string if part is specified, containing the requested part
+//          object otherwise, containing all parts and the queryKeys object
+function parseUri(URL, part) {
+  var matches = /^(([^:]+(?::|$))(?:(?:[^:]+:)?\/\/)?(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))((?:[^?#\/]*\/)*[^?#]*)(\?[^#]*)?(\#.*)?/.exec(URL) || [];
+  var uri = {};
+  // The key values are identical to the JS location object values for that key
+  var keys = ["href", "origin", "protocol", "host", "hostname", "port",
+              "pathname", "search", "hash"];
+
+  var partIndex = keys.indexOf(part);
+  if (partIndex === -1) {
+    for (i=0; i<keys.length; i++)
+      uri[keys[i]] = matches[i] || "";
+  } else
+    return matches[partIndex] || "";
+
+  // queryKeys contains all properties in uri.search and their values
+  // e.g., ?hello=world&ext=adblock would become {hello:"world", ext:"adblock"}
+  uri.queryKeys = {};
+  uri.search.replace(/(?:^\?|&)([^&=]*)=?([^&]*)/g, function () {
+    if (arguments[1]) uri.queryKeys[arguments[1]] = arguments[2];
+  });
+
+  return uri;
+};
+
+// TODO: move back into background.html since Safari can't use this
+// anywhere but in the background.  Do it after merging 6101 and 6238
+// and 5912 to avoid merge conflicts.
+// Inputs: key:string.
+// Returns value if key exists, else undefined.
+storage_get = function(key) {
+  var store = (window.SAFARI ? safari.extension.settings : localStorage);
+  var json = store.getItem(key);
+  if (json == null)
+    return undefined;
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    log("Couldn't parse json for " + key);
+    return undefined;
+  }
+}
+
+// Inputs: key:string, value:object.
+// Returns undefined.
+storage_set = function(key, value) {
+  var store = (window.SAFARI ? safari.extension.settings : localStorage);
+  try {
+    store.setItem(key, JSON.stringify(value));
+  } catch (ex) {
+    // Safari throws this error for all writes in Private Browsing mode.
+    // TODO: deal with the Safari case more gracefully.
+    if (ex.name == "QUOTA_EXCEEDED_ERR" && !SAFARI) {
+      alert(translate("storage_quota_exceeded"));
+      openTab("options/index.html#ui-tabs-2");
+    }
+  }
+}
